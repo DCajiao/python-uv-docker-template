7275ca31212a845ad18aa4ca46817b1838e683f2
DCajiao <dcajiaolazt@gmail.com>
2025-12-25 19:25:44 -0500

    ✨ feat: refactor main application structure to use Flask and register blueprints

--- src/main.py
+++ src/main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from python-uv-docker-template!")
+from flask import Flask
+
+import routes.health as health_routes
+from security.credentials_manager import CredentialsManager
+from routes.stats import stats_bp
+
+
+CREDENTIALS = CredentialsManager.get_credentials()
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # register blueprints
+    app.register_blueprint(health_routes.health_bp, url_prefix="/api")
+    app.register_blueprint(stats_bp, url_prefix="/api")
+
+    return app
 
 
 if __name__ == "__main__":
-    main()
+    app = create_app()
+    app.run(debug=True, host="0.0.0.0", port=int(
+        CREDENTIALS.get("PORT", 5000)))
